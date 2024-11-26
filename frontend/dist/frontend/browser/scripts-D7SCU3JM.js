da3afed84eef5c1390cb2631153cf665da62f346
Jair Plomares <jairo1433@hotmail.com>
2024-11-26 14:09:28 -0600

    carpeta dist

---
+++ frontend/dist/frontend/browser/scripts-D7SCU3JM.js
@@ -0,0 +1,1 @@
+(function(){var e=new Date().getFullYear();document.querySelector("#currentYear").innerHTML=e})();
---
+++ frontend/dist/frontend/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-A2NCQ22Q.mjs";import"./chunk-H55LMZF7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
