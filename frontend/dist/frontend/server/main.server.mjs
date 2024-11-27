47582505ec628e86dee3ae8537d16121456441dd
Jair Plomares <jairo1433@hotmail.com>
2024-11-27 17:32:24 -0600

    bootstrap local y favicon

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-A2NCQ22Q.mjs";import"./chunk-H55LMZF7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-65OESS2W.mjs";import"./chunk-M3LZ5EGQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
