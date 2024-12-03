55d865b440e092da5bcee127cb7d43c2fd3a0282
Jair Plomares <jairo1433@hotmail.com>
2024-12-03 17:37:07 -0600

    correccion footer y navbar

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-65OESS2W.mjs";import"./chunk-M3LZ5EGQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-LO23ZSEN.mjs";import"./chunk-25OYAUPO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
